4a4cf505cf288d3be51074d21e4b4f92d03fd481
Chaycoding <chasith1@gmail.com>
2024-01-15 08:28:45 +0530

    Updates

--- src/components/locationProvider.jsx
+++
@@ -1,7 +0,0 @@
-import { AnimatePresence } from "framer-motion";
-
-function Locationprovider({ children }) {
-  return <AnimatePresence mode="wait">{children}</AnimatePresence>;
-}
-
-export default Locationprovider;
